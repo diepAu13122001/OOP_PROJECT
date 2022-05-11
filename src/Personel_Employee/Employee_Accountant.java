8ed20d2159c57f36aaf1299691a8ee34c77a3a55
Đặng Thị Thùy Phương <87275526+thuyphuongg@users.noreply.github.com>
2022-05-11 15:31:18 +0700

    Update Employee_Accountant.java

--- src/Personel_Employee/Employee_Accountant.java
+++ src/Personel_Employee/Employee_Accountant.java
@@ -1,6 +1,6 @@
 package Personel_Employee;
 
-public class Employee_Accountant implements DraftObserverInterface {
+public class Employee_Accountant implements ObserverInterface {
 @Override
 	public void updateSalary(Employee emp) {
 		emp.setSalary((double) (emp.getSalary()+emp.getSalary()*0.1));
