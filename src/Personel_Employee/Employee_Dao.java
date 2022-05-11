1553892551bcc990f42d366bb20c30345884f952
Đặng Thị Thùy Phương <87275526+thuyphuongg@users.noreply.github.com>
2022-05-11 17:49:32 +0700

    Update Employee_Dao.java

--- src/Personel_Employee/Employee_Dao.java
+++ src/Personel_Employee/Employee_Dao.java
@@ -2,15 +2,15 @@ package model.personel_employee;
 
 import java.util.*;
 
-public class Employee_Dao {
+public class Employee_Dao  {
 	private List<Employee> list = new ArrayList<Employee>();
 
 	public static List<Employee> listNv() {
 		java.util.List<Employee> list = new ArrayList<Employee>();
 
-		Employee nv1 = new Employee("E01", "Tom", 200.0, 1, null);
-		Employee nv2 = new Employee("E02", "Jerry", 100.2, 2, null);
-		Employee nv3 = new Employee("E03", "Donald", 150.0, 2, null);
+		Employee nv1 = new Employee_Accountant("E01", "Tom", 200.0, "Accountant", null);
+		Employee nv2 = new Employee_HRM("E02", "Jerry", 100.2, "HRM", null);
+		Employee nv3 = new Employee_MenuManager("E03", "Donald", 200.2,"Menu_Manager", null);
 		list.add(nv1);
 		list.add(nv2);
 		list.add(nv3);
@@ -18,12 +18,11 @@ public class Employee_Dao {
 		return list;
 	}
 
-	public void addToList(String id, String name, double salary, int grade, double bonus) {
-		Employee nv = new Employee(id, name, salary, grade, bonus);
-		this.list.add(nv);
-	}
 
-	public List<Employee> getList() {
-		return this.list;
+	public static void main(String[] args) {
+		System.out.println(listNv().get(2).updateSalary().toString());
+		Employee e1 = new Employee_MenuManager("E04","Jerry",1000.2,"Menu_manager",null);
+		listNv().add(e1);
+		System.out.println(listNv().add(e1));
 	}
 }
