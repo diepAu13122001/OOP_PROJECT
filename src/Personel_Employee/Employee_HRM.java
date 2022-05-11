1fca26a674e8df9da9e96e86bd16bb1aa6b88009
Đặng Thị Thùy Phương <87275526+thuyphuongg@users.noreply.github.com>
2022-05-11 17:52:55 +0700

    Update Employee_HRM.java

--- src/Personel_Employee/Employee_HRM.java
+++ src/Personel_Employee/Employee_HRM.java
@@ -1,10 +1,14 @@
 package Personel_Employee;
 
-public class Employee_HRM implements ObserverInterface {
-	@Override
-	public void updateSalary(Employee emp) {
-		emp.setSalary(emp.getSalary()+40000);
-		
+public class Employee_HRM extends Employee {
+
+	public Employee_HRM(String empNo, String empName, Double salary, String position, Double bonus) {
+		super(empNo, empName, salary, position, bonus);
+		// TODO Auto-generated constructor stub
 	}
 
-}
+	@Override
+	public Double updateSalary() {
+		return this.setSalary(this.getSalary()+40000);
+	}
+
