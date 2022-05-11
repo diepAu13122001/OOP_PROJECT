465e98b13e89164cabe02e5fce36c03c26326969
Đặng Thị Thùy Phương <87275526+thuyphuongg@users.noreply.github.com>
2022-05-11 15:34:47 +0700

    Update Employee_MenuManager.java

--- src/Personel_Employee/Employee_MenuManager.java
+++ src/Personel_Employee/Employee_MenuManager.java
@@ -1,5 +1,11 @@
 package Personel_Employee;
 
-public class Employee_MenuManager {
+public class Employee_MenuManagerimplements ObserverInterface {
 
+	@Override
+	public void updateSalary(Employee emp) {
+		emp.setSalary(0.0);
+		
+	}
+			
 }
